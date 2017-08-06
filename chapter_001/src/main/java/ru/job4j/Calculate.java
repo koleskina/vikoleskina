2b84f65b107b9f46f6653121f23d5a91cfd74649
Viktoria Koleskina <viktoriakoleskina@yandex.ru>
2017-08-06 16:46:36 +0300

    1.3. JavaDoc. Code convention

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,6 +1,15 @@
+/**
+JavaDoc для класса ru.job4j.
+**/
+
 package ru.job4j;
 
 public class Calculate{
+
+/**
+JavaDoc для метода main.
+**/
+
 	public static void main(String[] args){
 	System.out.println("Hello world");
 
