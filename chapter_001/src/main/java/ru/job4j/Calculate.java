153d0df05f320d26ec8b648460b6741cdad28864
Viktoria Koleskina <viktoriakoleskina@yandex.ru>
2017-08-08 17:21:36 +0300

    1.5. Checkstyle

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,21 +1,19 @@
-/** @author Viktoria Koleskina (mailto:viktoriakoleskina@yandex.ru)
-* @version $Id$
-* @since 0.1**/
-
-/**
-JavaDoc for class ru.job4j.
-**/
 
 package ru.job4j;
 
-public class Calculate{
-
 /**
-JavaDoc for method main.
-**/
-
-	public static void main(String[] args){
-	System.out.println("Hello world");
-
-	}
+ * Calculate.
+ *
+ * @author Viktoria Koleskina (mailto:viktoriakoleskina@yandex.ru)
+* @version $Id$
+* @since 0.1
+ */
+public class Calculate {
+    /**
+     * Main.
+     * @param args - args.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
 }
