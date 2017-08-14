f0e3f1946da4448e83933e8c1b5c12c1d1f09361
Viktoria Koleskina <viktoriakoleskina@yandex.ru>
2017-08-15 00:44:22 +0300

    Added Hello World

---
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -0,0 +1,22 @@
+/** @author Viktoria Koleskina (mailto:viktoriakoleskina@yandex.ru)
+* @version $Id$
+* @since 0.1**/
+	package ru.job4j.max;
+/**
+ * Max.
+ **/
+	public class Max {
+/**
+* @param maxValue.
+*/
+    private int maxValue;
+/**
+* @param first **first parameter**
+* @param second **sec parameter**
+* @return **sec parameter**
+     */
+	 public int max(int first, int second) {
+		this.maxValue = first < second ? second : first;
+		return this.maxValue;
+		}
+}
