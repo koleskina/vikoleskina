552138c9ecdd28a740a586ba460bb4ebec5c4d36
vkoleskina <viktoriakoleskina@yandex.ru>
2017-08-15 17:35:01 +0400

    3.2. Положение точки

---
+++ chapter_001/src/main/java/ru/job4j/condition/package-info.java
@@ -0,0 +1,12 @@
+/** @author Viktoria Koleskina (mailto:viktoriakoleskina@yandex.ru)
+* @version $Id$
+* @since 0.1**/
+
+/**
+* Package for calculate task.
+*
+* @author Viktoria Koleskina (mailto:viktoriakoleskina@yandex.ru)
+* @version $Id$
+* @since 0.1
+*/
+package ru.job4j;
