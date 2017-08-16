33e4a53f96c7281af0f602a2c5ed7a55570be17c
vkoleskina <viktoriakoleskina@yandex.ru>
2017-08-16 18:59:33 +0400

    3.3. Вычисления площади треугольника

--- chapter_001/src/test/java/ru/job4j/condition/TriangleTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/TriangleTest.java
@@ -1,7 +1,7 @@
 /** @author Viktoria Koleskina (mailto:viktoriakoleskina@yandex.ru)
 * @version $Id$
 * @since 0.1**/
-	package ru.job4j.condition;
+package ru.job4j.condition;
 import org.junit.Test;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
