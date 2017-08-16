2a33585bde385310de534f0f622206a994d5b9af
Viktoria Koleskina <viktoriakoleskina@yandex.ru>
2017-08-17 00:23:52 +0300

    sleeep

--- chapter_001/src/main/java/ru/job4j/condition/Triangle.java
+++ chapter_001/src/main/java/ru/job4j/condition/Triangle.java
@@ -46,21 +46,17 @@ public class Triangle {
 						/** @param left Точка слева
 						* @param right Точка с права.
 						* @return расстояние между точками left и right.*/
-public double distance(Point left, Point right) { 				
-    return ab = Math.sqrt((xb - xa)^2) + ((yb - ya)^2); //расстояние между точками а и b
-	return ac = Math.sqrt((xc - xa)^2) + ((yc - ya)^2); //расстояние между точками а и c
-	return bc = Math.sqrt((xc - xb)^2) + ((yc - yb)^2); //расстояние между точками b и c
+public double distance(Point left, Point right) { 	
+return double distance = Math.sqrt (Math.pow(right.getX() - left.getX(), 2) + (Math.pow(right.getY() - left.getY(), 2)));
 }
 						/** @param ab расстояние между точками a b
 						* @param ac расстояние между точками a c
 						* @param bc расстояние между точками b c
 						* @return Периметр.*/
 public double period(double ab, double ac, double bc) { 		
-    this.ab = ab;
-	this.ac = ac;
-	this.bc = bc;
 	return (ab + ac + bc) / 2;
 }
+public double area() {
     double rsl = -1;
     double ab = this.distance(this.a, this.b);
     double ac = this.distance(this.a, this.c);
