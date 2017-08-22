760f6631e0136c74fa2dca98f8f99466120f4d74
vkoleskina <viktoriakoleskina@yandex.ru>
2017-08-22 15:34:29 +0400

    Added Hello World

--- chapter_001/src/main/java/ru/job4j/max/Max.java
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -1,22 +1,31 @@
-/** @author Viktoria Koleskina (mailto:viktoriakoleskina@yandex.ru)
-* @version $Id$
-* @since 0.1**/
-	package ru.job4j.max;
-/**
- * Max.
- **/
-	public class Max {
-/**
-* @param maxValue.
-*/
-    private int maxValue;
-/**
-* @param first **first parameter**
-* @param second **sec parameter**
-* @return **return return**
-     */
-	 public int max(int first, int second) {
-		this.maxValue = first < second ? second : first;
-		return this.maxValue;
-		}
+/** @author Viktoria Koleskina (mailto:viktoriakoleskina@yandex.ru) 
+* @version $Id$ 
+* @since 0.1**/ 
+package ru.job4j.max; 
+/** 
+* Max. 
+**/ 
+public class Max { 
+/** 
+* @param maxValue. 
+*/ 
+private int maxValue; 
+/** 
+* @param first **first parameter** 
+* @param second **sec parameter** 
+* @return **return return** 
+*/ 
+public int max(int first, int second) { 
+this.maxValue = first < second ? second : first; 
+return this.maxValue; 
+} 
+/** 
+* @param first **first parameter** 
+* @param second **sec parameter** 
+* @param third **third parameter** 
+* @return **return Max Value** 
+*/ 
+public int max(int first, int second, int third) { 
+return this.max(first, this.max(second, third)); 
+} 
 }
