00fa426a5d696e3692667358fab50ac77ab3f408
vkoleskina <viktoriakoleskina@yandex.ru>
2017-08-24 17:35:24 +0400

    Board

---
+++ chapter_001/src/main/java/ru/job4j/loop/Board.java
@@ -0,0 +1,14 @@
+/** @author Viktoria Koleskina (mailto:viktoriakoleskina@yandex.ru)
+* @version $Id$
+* @since 0.1**/
+package ru.job4j.loop;
+/**
+* Board.
+**/
+public class Board {
+public String paint(int width, int height){
+	
+	}	
+}
+
+
