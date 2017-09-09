bd65dbf65acde2f3dd487daa5c77d06add743d6b
Viktoria Koleskina <viktoriakoleskina@yandex.ru>
2017-09-09 18:05:45 +0300

    7.3. Проверка, что одно слово находится в другом слове

---
+++ chapter_001/src/main/java/ru/job4j/array/Check.java
@@ -0,0 +1,38 @@
+/** @author Viktoria Koleskina (mailto:viktoriakoleskina@yandex.ru)
+ * @version $Id$
+ * @since 0.1**/
+package ru.job4j.array;
+
+import java.util.Arrays;
+
+/**
+ * ArrayDuplicate.
+ * 
+ **/
+public class Check {
+	/**
+	 * * @param array massive
+	 * @return return return
+	 */
+	boolean contains(String origin, String sub){
+		boolean vi = false;
+		char[] or = origin.toCharArray();
+		char[] su = sub.toCharArray();
+		int count = 0;
+		for (int i = 0; i < or.length; i++){
+				for (int j = 0; j < su.length; j++){
+					if (or[i + j] == su[j]){
+						count++;
+					}
+				}
+			if (su.length == count){
+				vi=true;
+				break;
+			}
+			else {
+				count = 0;
+			}
+		}
+		return vi;
+	}
+}
