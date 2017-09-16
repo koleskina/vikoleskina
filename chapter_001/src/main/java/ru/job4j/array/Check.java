ef93802135d9c64edac4ec3f254e457c7ded2ccb
Viktoria Koleskina <viktoriakoleskina@yandex.ru>
2017-09-16 23:14:20 +0300

    7.3. Проверка, что одно слово находится в другом слове

--- chapter_001/src/main/java/ru/job4j/array/Check.java
+++ chapter_001/src/main/java/ru/job4j/array/Check.java
@@ -2,34 +2,30 @@
  * @version $Id$
  * @since 0.1**/
 package ru.job4j.array;
-
-import java.util.Arrays;
-
 /**
  * ArrayDuplicate.
- * 
  **/
 public class Check {
 	/**
-	 * * @param array massive
+	 * * @param origin origin
+	 *  @param sub sub
 	 * @return return return
 	 */
-	boolean contains(String origin, String sub){
+	boolean contains(String origin, String sub) {
 		boolean vi = false;
 		char[] or = origin.toCharArray();
 		char[] su = sub.toCharArray();
 		int count = 0;
-		for (int i = 0; i < or.length; i++){
-				for (int j = 0; j < su.length; j++){
-					if (or[i + j] == su[j]){
+		for (int i = 0; i < or.length; i++) {
+				for (int j = 0; j < su.length; j++) {
+					if (or[i + j] == su[j]) {
 						count++;
 					}
 				}
-			if (su.length == count){
-				vi=true;
+			if (su.length == count) {
+				vi = true;
 				break;
-			}
-			else {
+			} else {
 				count = 0;
 			}
 		}
