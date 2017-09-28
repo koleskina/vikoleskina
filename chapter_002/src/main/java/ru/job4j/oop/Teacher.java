fdc043cb15757fd07268f840cb0619cd17cef379
vkoleskina <viktoriakoleskina@yandex.ru>
2017-09-28 12:51:06 +0400

    1.Реализация профессий в коде

---
+++ chapter_002/src/main/java/ru/job4j/oop/Teacher.java
@@ -0,0 +1,37 @@
+package ru.job4j.oop;
+/**
+ * Teacher.
+ **/
+public class Teacher extends Profession {
+    /*** @param discipline.*/
+    private String discipline = new String();
+    /*** @param school.*/
+    private int  school;
+    /*** Teacher.**/
+    public Teacher() {
+    }
+
+    /*** Teacher.
+     * @param name name
+     * @param institute institute
+     * @param yearOfIssue yearOfIssue
+     * @param city  city
+     * @param workExperience workExperience
+     * @param discipline discipline
+     * @param school school
+     */
+    public Teacher(String name, String institute, int yearOfIssue, String city, int workExperience, String discipline,  int school) {
+        super(name, institute, yearOfIssue, city, workExperience);
+        this.discipline = discipline;
+        this.school =  school;
+    }
+    /**
+     *  @param doc doc
+     * @return return return
+     */
+    public String teach(Doctor doc) {
+        String tea = "Учитель " + this.name + " обучает " + doc.getName();
+        return tea;
+    }
+}
+
